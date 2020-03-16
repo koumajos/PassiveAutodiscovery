4bc7342c94638f0c63e1334d02bfa6d59e6adc84
koumajos <61184328+koumajos@users.noreply.github.com>
2020-03-16 11:15:06 -0400

    Analyzer: 1%

--- Analyzer.py
+++ Analyzer.py
@@ -4,17 +4,23 @@ import os
 import ipaddress
 import sqlite3
 
+    
+def AnalyzeLocalDevice(LocalDevice, cursor, SQLiteConnection):
+    print("Device: ", LocalDevice[0])
 #=======================================================================================================================================
 #Main function of Analyzer
 def DoAnalyze(SQLiteConnection):
-    None
+    cursor = SQLiteConnection.cursor()
+    cursor.execute("SELECT * FROM LocalDevice")
+    LocalDevices = cursor.fetchall()
+    for LocalDevice in LocalDevices:
+        AnalyzeLocalDevice(LocalDevice, cursor, SQLiteConnection)
 #=======================================================================================================================================
 # Main loop
 try:    #connect to a database
-    print("Connecting to a database....")
-    if os.path.exists('Database.db'):
-        print("", end='')
-    else:
+    print("Connecting to a database....", end='')
+    if not os.path.exists('Database.db'):
+        print("")
         print("can't connect to Database.db")
         sys.exit()
     SQLiteConnection = sqlite3.connect('Database.db')
